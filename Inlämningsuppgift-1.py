06c9178966b7a8ceca2ff071f1e49fb1e78f7460
Rasmus Dahlberg <rasmus.dahlberg2000@gmail.com>
2023-09-27 13:55:07 +0200

    Update Inlämningsuppgift-1.py

--- Inlämningsuppgift-1.py
+++ Inlämningsuppgift-1.py
@@ -4,9 +4,12 @@ If you need to change the path of the file you can do that in the "input_file" v
 """
 
 # We need to import io so that we can read and write in utf-8
-import io
-import os
+# The OS import is just so that we can check if the file exists and if the file contains anything
+from io import open
+from os import path
 
+# I ddefine every variable that I might want to change.
+# This is also future proofing if we were to extend the current code to something bigger
 class fileInfo:
     def __init__(self, input_File_Name = "", output_File_Name = "output.txt", consonants = "bcdfghjklmnpqrstvzx", translated_Text = "", original_Text = any):
         self._input_File_Name = input_File_Name
@@ -15,6 +18,7 @@ class fileInfo:
         self._translated_Text = translated_Text
         self._original_Text = original_Text
     
+    # I am creating get and set functions for each variable to access them
     def get_input_File_Name(self):
         return self._input_File_Name
     
@@ -36,6 +40,7 @@ class fileInfo:
     def get_translated_Text(self):
         return self._translated_Text
     
+    # This is the only odd one where I want to add a character to a string. This will be the translated text
     def add_translated_Text(self, x):
         self._translated_Text += x
 
@@ -50,11 +55,14 @@ file_info = fileInfo()
 
 # This function will handle the actual translation of the text
 def translate():
-    # This for loop will check every char individually from the text file
+    
+    # This "for loop" will check every char individually from the text file
     for char in file_info.get_original_Text():
+
         # If we find that the char exists in the consonant variable then we can change it to rövarspråk
         if char.lower() in file_info.get_consonants():
             file_info.add_translated_Text(char + "o" + char)
+
         # Else we will just add the char to the translated string as nothing needs to be done to it
         else:
             file_info.add_translated_Text(char)
@@ -63,12 +71,14 @@ def translate():
 
 # This function handles the input and output file with read and write
 def translate_file():
+
     # We are using try except if the file does not exsist and will print the OSError is that is the case
     try:
-        if os.path.isfile(file_info.get_input_File_Name()) and os.path.getsize(file_info.get_input_File_Name()) > 0:
+        
+        if path.getsize(file_info.get_input_File_Name()) > 0:
 
             # Here we initiate the file with the io import so we can read åäö in the file
-            with io.open(file_info.get_input_File_Name(), mode="r", encoding="utf-8") as file:
+            with open(file_info.get_input_File_Name(), mode="r", encoding="utf-8") as file:
 
                 # Send the file content to the other function so it can be translated
                 file_info.set_original_Text(file.read())
@@ -77,7 +87,7 @@ def translate_file():
                 translate()
             
             # We also need to use io here to write using utf-8 so that we can write åäö to the files
-            with io.open(file_info.get_output_File_Name(), mode="w", encoding="utf-8") as file:
+            with open(file_info.get_output_File_Name(), mode="w", encoding="utf-8") as file:
                 file.write(file_info.get_translated_Text())
                 print(f'This is the tanslated text:\n{file_info.get_translated_Text()}')
 
@@ -85,14 +95,18 @@ def translate_file():
         else:
             print('There is nothing to translate')
 
-    # catch the OSError is the file does not exsist
+
+    # catch the OSError if the file does not exsist
     except OSError as error:
         print(error)
 
-
+# This will be the first ting to run in the file
 if __name__ == "__main__":
+
     # Add the file to the class so that we can access it anywhere
-    file_info.set_input_File_Name("svenskt_text.txt")
+    file_info.set_input_File_Name(input('Write the name of the file that should be translated. Remember to incluse ".txt" at the end'))
+    while not path.isfile(file_info.get_input_File_Name()):
+        file_info.set_input_File_Name(input(f'The file: "{file_info.get_input_File_Name()} does not exist"\nPlease write another file: '))
 
     # Start cheking and initializing the file given
     translate_file()
